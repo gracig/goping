78de56b76d9eb970c62aab20e6df9c9381822dde
gracig <gerson.graciani@gmail.com>
2016-06-21 23:53:02 -0300

    Reading flags from command line (starting)

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/gracig/goping/protocol"
-)
+import "flag"
 
 func main() {
-	im := protocol.IcmpMessage{}
-	im.Identifier = 2
-	im.Sequence = 1
-	im.Type = protocol.IcmpMessage_ECHO_REPLY
-	im.Code = &protocol.IcmpMessage_SingleCode_{}
-	fmt.Printf("Not Yet Functional! .ICMP Message  %#v\n", im)
-
+	flag.PrintDefaults()
 }
