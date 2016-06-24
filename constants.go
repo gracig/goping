4bea17a45dc202948a1bfb9a16230a19325d65e7
GITNAME <gerson.graciani@gmail.com>
2016-06-24 01:58:55 +0000

    fixed build

--- constants.go
+++ constants.go
@@ -5,7 +5,7 @@ const (
 	DEFDATALEN  = 64 - 8 //default data length
 	MAXIPLEN    = 60
 	MAXICMPLEN  = 76
-	MAXPACKet   = (65536 - 60 - 8) //max packet size
+	MAXPACKET   = (65536 - 60 - 8) //max packet size
 	MAXWAIT     = 10               //max seconds to wait for a response
 	NROUTES     = 9                //Number of record route slots
 )
