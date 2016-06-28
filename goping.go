e07cb0e12c0316636da9a0ad26a03bfac1a7db5b
GITNAME <gerson.graciani@gmail.com>
2016-06-28 10:50:36 +0000

    Still creating goping.
    Need to figure out timeout

--- goping.go
+++ goping.go
@@ -2,6 +2,7 @@ package goping
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/gracig/goshared/log"
@@ -17,7 +18,10 @@ const (
 )
 
 type Ping struct {
-	Host string
+	Host  string
+	Count uint
+	Sent  uint
+	Recv  uint
 }
 
 type Pong struct {
@@ -25,11 +29,15 @@ type Pong struct {
 	RTT float64
 }
 
-func Add() {
+type seqPing struct {
+	seq      uint64
+	ping     Ping
+	onPong   func(ping Ping, pong Pong)
+	onFinish func(ping Ping)
 }
 
 var (
-	chPing   chan Ping     = make(chan Ping)
+	chPing   chan seqPing  = make(chan seqPing)
 	chPong   chan Pong     = make(chan Pong)
 	chTicker *time.Ticker  = time.NewTicker(time.Second * 1)
 	chPause  chan struct{} = make(chan struct{})
@@ -38,14 +46,31 @@ var (
 
 	paused  bool = false
 	pausedM sync.Mutex
+	seq     uint64
 )
+
+func Add(ping Ping, onPong func(ping Ping, pong Pong), onFinish func(ping Ping)) error {
+	sequence := atomic.AddUint64(&seq, 1) % MAX_SEQUENCE
+	chPing <- seqPing{sequence, ping, onPong, onFinish}
+	return nil
+}
 
 func Run() {
 	//Work with all states inside this function
+
+	//var seqstore []Ping = make([]Ping, MAX_SEQUENCE, MAX_SEQUENCE)
+
 	for {
 		select {
-		case <-chPing:
+		case p := <-chPing:
 			log.Info.Printf("Called ping\n")
+			if p.ping.Sent < p.ping.Recv {
+				p.ping.Sent++
+				//Make a ping
+			} else {
+				//Finish Ping
+
+			}
 		case <-chPong:
 			log.Info.Printf("Called pong\n")
 		case <-chTicker.C:
