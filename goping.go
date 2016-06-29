c267a1fd1a066ffcf32abef76b7fad83139d1f4d
GITNAME <gerson.graciani@gmail.com>
2016-06-29 01:13:48 +0000

    Added Timeout functionality. The tests are working
    Next step is to mock a ping over the wire and test ping responses

--- goping.go
+++ goping.go
@@ -1,10 +1,10 @@
 package goping
 
 import (
+	"math"
 	"sync"
 	"sync/atomic"
 	"time"
-	"math"
 
 	"github.com/gracig/goshared/log"
 )
@@ -19,38 +19,90 @@ const (
 )
 
 type Ping struct {
-	Host  string
-	Count uint
-	Sent  uint
-	Recv  uint
+	Data     map[string]string
+	Host     string
+	Timeout  uint
+	Interval uint
+	Count    uint
+
+	Sent   float64
+	Recv   float64
+	Failed float64
+	AvgRtt float64
+	MaxRtt float64
+	MinRtt float64
+
+	ctx Context
 }
 
 type Pong struct {
-	seq uint64
-	RTT float64
+	Seq         uint
+	Size        uint
+	RTT         float64
+	ICMPType    uint
+	ICMPSubtype uint
+	Timedout    bool
 }
 
-type seqPing struct {
-	ping     Ping
-	onPong   func(ping Ping, pong Pong)
-	onFinish func(ping Ping)
+//Represent a response Tuple
+type PingPong struct {
+	Ping
+	Pong
+	Done bool
+}
+
+type Context interface {
+	PingPongChannel() <-chan PingPong
+	pingPongChannel() chan PingPong
+	addPing(delta int)
+	pingCount() int
+}
+
+type context struct {
+	chpp  chan PingPong
+	count int
+}
+
+func (c *context) PingPongChannel() <-chan PingPong {
+	return c.chpp
+}
+func (c *context) pingPongChannel() chan PingPong {
+	return c.chpp
+}
+func (c *context) addPing(delta int) {
+	c.count += delta
+}
+func (c *context) pingCount() int {
+	return c.count
+}
+
+func NewContext() Context {
+	return &context{
+		chpp: make(chan PingPong),
+	}
+}
+
+type control struct {
+	paused bool
+	sync.Mutex
 }
 
 var (
-	chPing   chan seqPing  = make(chan seqPing)
+	//Channels to control
+	chPing   chan Ping     = make(chan Ping)
 	chPong   chan Pong     = make(chan Pong)
-	chTicker *time.Ticker  = time.NewTicker(time.Second * 1)
 	chPause  chan struct{} = make(chan struct{})
 	chResume chan struct{} = make(chan struct{})
 	chDone   chan struct{} = make(chan struct{})
 
-	paused  bool = false
-	pausedM sync.Mutex
-	seq     uint64
+	ctrl = control{paused: false}
+
+	seq uint64
 )
 
-func Add(ping Ping, onPong func(ping Ping, pong Pong), onFinish func(ping Ping)) error {
-	chPing <- seqPing{ping, onPong, onFinish}
+func Add(ping Ping, ctx Context) error {
+	ping.ctx = ctx
+	chPing <- ping
 	return nil
 }
 
@@ -61,39 +113,65 @@ func Run() {
 
 	for {
 		select {
-		case p := <-chPing:
-			log.Info.Printf("Called ping\n")
-			if p.ping.Sent < p.ping.Recv {
+
+		case ping := <-chPing:
+
+			if uint(ping.Sent) < ping.Count {
+				//If it is the first ping, adding context count
+				if ping.Sent == 0 {
+					ping.ctx.addPing(1)
+				}
+
 				//Prepare Ping
 				sequence := atomic.AddUint64(&seq, 1) % MAX_SEQUENCE
-				p.ping.Sent++
-				if (recvchans[sequence] != nil){
-					close (recvchans[sequence])
+				if recvchans[sequence] != nil {
+					close(recvchans[sequence])
 					recvchans[sequence] = nil
 				}
-				recvchans[sequence] = make (chan Pong,1) //The number avoid the channel to hold state
+				recvchans[sequence] = make(chan Pong, 1) //The number avoid the channel to hold state
 
 				//Make a ping
+				ping.Sent++
 
 				//Wait a response
-				go func(sequence uint64 ,p seqPing, chrecv chan Pong){
-					tout :=  time.NewTimer(time.Second * 3)
-					var pong Pong = Pong{ sequence, math.NaN()}
+				go func(sequence uint64, ping Ping, chrecv chan Pong) {
+
+					//Creates the timer for another ping
+					wait := time.NewTimer(time.Millisecond * time.Duration(ping.Interval))
+
+					//Wait for a Pong or times out
+					var pong Pong
+					tout := time.NewTimer(time.Millisecond * time.Duration(ping.Timeout))
 					select {
 					case <-tout.C:
+						pong = Pong{
+							Seq:      uint(sequence),
+							RTT:      math.NaN(),
+							Timedout: true,
+						}
 						log.Warn.Println("Timed out")
-					case pong= <-chrecv:
+					case pong = <-chrecv:
 					}
-					p.onPong(p.ping, pong)
-				}(sequence, p , recvchans[sequence])
+
+					//Send Ping Pong to user in a goroutine to not block function
+					ping.ctx.pingPongChannel() <- PingPong{ping, pong, false}
+
+					//Sends Ping to chPing after wait for interval
+					<-wait.C
+					chPing <- ping
+
+				}(sequence, ping, recvchans[sequence])
+
 			} else {
 				//Finish Ping
-
+				ping.ctx.pingPongChannel() <- PingPong{ping, Pong{}, true}
+				ping.ctx.addPing(-1)
+				if ping.ctx.pingCount() == 0 {
+					close(ping.ctx.pingPongChannel())
+				}
 			}
 		case <-chPong:
 			log.Info.Printf("Called pong\n")
-		case <-chTicker.C:
-			log.Info.Printf("Called ticker\n")
 		case <-chPause:
 			log.Info.Printf("Called Pause\n")
 			<-chResume
@@ -105,11 +183,11 @@ func Run() {
 }
 
 func Pause() {
-	pausedM.Lock()
-	defer pausedM.Unlock()
-	if !paused {
+	ctrl.Lock()
+	defer ctrl.Unlock()
+	if !ctrl.paused {
 		chPause <- struct{}{}
-		paused = true
+		ctrl.paused = true
 		log.Info.Println("goping was succesfully paused")
 	} else {
 		log.Warn.Println("You requested to pause  an already paused goping")
@@ -117,11 +195,11 @@ func Pause() {
 }
 
 func Resume() {
-	pausedM.Lock()
-	defer pausedM.Unlock()
-	if paused {
+	ctrl.Lock()
+	defer ctrl.Unlock()
+	if ctrl.paused {
 		chResume <- struct{}{}
-		paused = false
+		ctrl.paused = false
 		log.Info.Println("go ping was succesfully resumed")
 	} else {
 		log.Warn.Println("You requested to resume a non paused goping")
--- goping_test.go
+++ goping_test.go
@@ -2,39 +2,59 @@ package goping
 
 import (
 	"math/rand"
+	"sync"
 	"testing"
 	"time"
-	"sync"
 )
 
 func TestPauseResume(t *testing.T) {
+	loop := 1000
 	go Run()
 
 	rand.Seed(1)
 	var wg sync.WaitGroup
-	wg.Add(200)
+	wg.Add(loop)
 	go func() {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < loop/2; i++ {
 			Pause()
 			time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 			wg.Done()
 		}
 	}()
 	go func() {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < loop/2; i++ {
 			Resume()
 			time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
 			wg.Done()
 		}
 	}()
+
 	wg.Wait()
 
 }
 
-func TestTimeout(t *testing.T){
+func TestTimeout(t *testing.T) {
 	go Run()
-	p:=Ping{Host:"localhost",Count:10}
-	_ = p
+	ping := Ping{Host: "localhost", Count: 10, Timeout: 3000}
+	ctx := NewContext()
+	for i := 0; i < 100; i++ {
+		Add(ping, ctx)
+	}
 
+	done := make(chan struct{})
+
+	go func() {
+		for pp := range ctx.PingPongChannel() {
+			t.Logf("Received pp %v\n", pp)
+		}
+		done <- struct{}{}
+	}()
+
+	timer := time.NewTimer(time.Second * 60)
+	select {
+	case <-done:
+	case <-timer.C:
+		t.Error("Timeout took too long, something is wrong")
+	}
 
 }
