0508b0f9e1c81cfed1f7fc5fb49611acb7c951ac
GITNAME <gerson.graciani@gmail.com>
2016-07-05 11:25:05 +0000

    Refactorung: Adding interfaces
    TODO: Transform all communication into interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,7 +1,21 @@
 package goping
 
-type ICMPRequestBuilder interface {
+import (
+	"net"
+
+	"golang.org/x/net/icmp"
+)
+
+type ICMPRequest interface {
+	//Protocol: The ip protocol to send the ip message  (ICMP=1, TCP=6, UDP=17)
+	Protocol() uint
+
+	//IP: The target address of this request
+	IP() net.IP
+
+	//The ICMP Message to send
+	Message() icmp.Message
 }
 
-type ICMPRequestSender interface {
+type ICMPReply interface {
 }
