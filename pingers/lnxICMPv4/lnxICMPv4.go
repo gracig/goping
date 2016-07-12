4fa7fb4d3df922256f29a8f65293b3da401b36f6
GITNAME <gerson.graciani@gmail.com>
2016-07-12 12:00:24 +0000

    Making linux pinger

--- pingers/lnxICMPv4/lnxICMPv4.go
+++ pingers/lnxICMPv4/lnxICMPv4.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"sync"
 	"syscall"
 	"time"
@@ -23,6 +22,12 @@ func init() {
 //	Ping(r Request, seq int) (future <-chan RawResponse,err error)
 //}
 
+type ConnManager interface {
+	Open() (int, error)
+	Close(fd int) error
+	SendTo(fd int, p []byte, to syscall.Sockaddr) error
+	RecvMsg(fd int, buf []byte) (peer net.IP, when time.Time, err error)
+}
 type seqfut struct {
 	seq int
 	fut chan goping.RawResponse
@@ -38,6 +43,7 @@ type pinger struct {
 	chseqfut chan seqfut
 	chseqrep chan seqresp
 	chdone   chan struct{}
+	conn     ConnManager
 }
 
 func (p *pinger) Ping(r goping.Request, seq int) (future <-chan goping.RawResponse, err error) {
@@ -77,39 +83,19 @@ func (p *pinger) loop() {
 }
 
 func (p *pinger) Init() {
-	//Create a raw socket to read icmp packets
-	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_ICMP)
-	if err != nil {
-		log.Fatalf("Could not open ICMP RAW Socket: [%v]\n", err)
-	}
 
-	//Set the option to receive the kernel timestamp from each received message
-	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_TIMESTAMP, 1); err != nil {
-		log.Fatalf("Could not set sock opt syscall [%v]\n", err)
+	if fd, err := p.conn.Open(); err != nil {
+		log.Fatalf("Could not open connection: [%v]", err)
+	} else {
+		p.fd = fd
 	}
-
-	//Increase the socket buffer
-	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, 1024*1024); err != nil {
-		log.Fatalf("Could not set sock opt syscall Increase buffer: [%v]\n", err)
-	}
-
-	//Listen on all interfaces
-	var addr syscall.Sockaddr = &syscall.SockaddrInet4{
-		Port: 0,
-		Addr: [4]byte{0, 0, 0, 0},
-	}
-	if err := syscall.Bind(fd, addr); err != nil {
-		log.Fatalf("Could not bind all interfaces to socket: [%v]", err)
-	}
-
-	p.fd = fd
 	go p.loop()
-	go p.startListener(fd)
+	go p.startListener(p.fd)
 
 }
 
 func (p *pinger) Close() {
-	if err := syscall.Close(p.fd); err != nil {
+	if err := p.conn.Close(p.fd); err != nil {
 		log.Printf("Error while closing socket: [%v]", err)
 	}
 }
@@ -119,20 +105,16 @@ func (p *pinger) startListener(fd int) {
 	//Buffer to receive the ping packet
 	buf := make([]byte, 1024)
 
-	//Buffer to receive the control message
-	oob := make([]byte, 64)
-
 	//Variables used in the for loop
 	var pid, seq int
-	var tv syscall.Timeval
-	var bbuf bytes.Buffer
 
 	for {
 		//Receives a message from the socket sent by the kernel
-		_, oobn, _, peer, err := syscall.Recvmsg(fd, buf, oob, 0)
+
+		peer, when, err := p.conn.RecvMsg(fd, buf)
 		if err != nil {
 			log.Printf("Error reading icmp packet from the socket: %v\n", err)
-			return //Continue on error
+			continue //Continue on error
 		}
 
 		//Finds the pid and seq value.
@@ -156,33 +138,71 @@ func (p *pinger) startListener(fd int) {
 			continue
 		}
 
-		//Find the pinger object related to the Sequence value in the g_PINGER_RING
-		findPinger(&ps, seq)
-		if ps.r.Value == nil {
-			info.Println("Could not find pinger for sequence:", seq)
-			continue
-		}
-		p := ps.r.Value.(*pinger)
-		if p == nil {
-			info.Println("Could not receive pinger from g_REPLY_ARRAY")
-			continue
-		}
-		p.reply.WhenRecv = time.Now()
-		p.reply.Peer = net.IPv4(
-			peer.(*syscall.SockaddrInet4).Addr[0],
-			peer.(*syscall.SockaddrInet4).Addr[1],
-			peer.(*syscall.SockaddrInet4).Addr[2],
-			peer.(*syscall.SockaddrInet4).Addr[3],
-		)
-		p.reply.ICMPType = buf[20]
-		p.reply.ICMPCode = buf[21]
+		//Send the raw response back to channel
+		p.chseqrep <- seqresp{seq, goping.RawResponse{ICMPMessage: buf[:40], Peer: peer, RTT: 0}}
 
-		//Parses the Control Message to find the SO_TIMESTAMP value
-		cmsgs, err := syscall.ParseSocketControlMessage(oob[:oobn])
-		if err != nil {
-			severe.Println(os.NewSyscallError("parse socket control message", err))
-		}
+	}
 
+}
+
+/***Conn Manager Implementation ***/
+type connmanager struct{}
+
+func (c *connmanager) Open() (int, error) {
+	//Create a raw socket to read icmp packets
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_ICMP)
+	if err != nil {
+		return 0, err
+	}
+
+	//Set the option to receive the kernel timestamp from each received message
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_TIMESTAMP, 1); err != nil {
+		return 0, err
+	}
+
+	//Increase the socket buffer
+	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, 1024*1024); err != nil {
+		return 0, err
+	}
+
+	//Listen on all interfaces
+	var addr syscall.Sockaddr = &syscall.SockaddrInet4{
+		Port: 0,
+		Addr: [4]byte{0, 0, 0, 0},
+	}
+	if err := syscall.Bind(fd, addr); err != nil {
+		return 0, err
+	}
+	return fd, nil
+
+}
+func (c *connmanager) Close(fd int) error {
+	return syscall.Close(fd)
+}
+
+func (c *connmanager) SendTo(fd int, p []byte, to syscall.Sockaddr) error {
+	return syscall.Sendto(fd, p, 0, to)
+}
+func (c *connmanager) RecvFrom(fd int, buf []byte) (peer net.IP, when time.Time, rerr error) {
+
+	//Buffer to receive the control message
+	oob := make([]byte, 64)
+
+	//Receive Message from socket
+	n, oobn, recvflags, from, err := syscall.Recvmsg(fd, buf, oob, 0)
+	if err != nil {
+		rerr = err
+		return
+	}
+
+	//Parses the Control Message to find the SO_TIMESTAMP value
+	if cmsgs, err := syscall.ParseSocketControlMessage(oob[:oobn]); err != nil {
+		rerr = err
+		return
+	} else {
+
+		var bbuf bytes.Buffer
+		var tv syscall.Timeval
 		//Iterate over the control messages
 		for _, m := range cmsgs {
 			//Continue if control message is not syscall.SOL_SOCKET
@@ -195,15 +215,19 @@ func (p *pinger) startListener(fd int) {
 				bbuf.Write(m.Data)
 				binary.Read(&bbuf, binary.LittleEndian, &tv)
 				bbuf.Reset()
-				p.reply.WhenRecv = time.Unix(tv.Unix())
+				when = time.Unix(tv.Unix())
 			}
 		}
-
-		//Send the pong object  back to the caller
-		go func(p *pinger) {
-			p.chPong <- p
-			debug.Println("Sent pong reference to the ping.pongchan reference ")
-		}(p)
 	}
+
+	//Get peer address
+	peer = net.IPv4(
+		from.(*syscall.SockaddrInet4).Addr[0],
+		from.(*syscall.SockaddrInet4).Addr[1],
+		from.(*syscall.SockaddrInet4).Addr[2],
+		from.(*syscall.SockaddrInet4).Addr[3],
+	)
+
+	return
 
 }
