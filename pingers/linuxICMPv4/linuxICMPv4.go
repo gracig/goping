aa2f7b3171d8ccc3245c7636a6831cc1c74506e1
GITNAME <gerson.graciani@gmail.com>
2016-07-26 02:45:59 +0000

    Refactoring linuxICMPv4Pinger. Unfinished work

--- pingers/linuxICMPv4/linuxICMPv4.go
+++ pingers/linuxICMPv4/linuxICMPv4.go
@@ -1,10 +1,9 @@
-package lnxICMPv4
+package linuxICMPv4
 
 import (
 	"bytes"
 	"encoding/binary"
 	"errors"
-	"log"
 	"math"
 	"net"
 	"syscall"
@@ -14,7 +13,7 @@ import (
 )
 
 func init() {
-	goping.RegPingerAdd("lnxICMPv4", new(pinger))
+	goping.RegPingerAdd("linuxICMPv4", new(pinger))
 }
 
 //Pinger is responsible for send and receive pings over the network
@@ -25,8 +24,8 @@ func init() {
 type ConnManager interface {
 	Open() (int, error)
 	Close(fd int) error
-	SendTo(fd int, p []byte, to syscall.Sockaddr) error
-	RecvMsg(fd int, buf []byte) (peer net.IP, when time.Time, err error)
+	Sendto(fd int, p []byte, flags int, to syscall.Sockaddr) (err error)
+	Recvmsg(fd int, p, oob []byte, flags int) (n, oobn int, recvflags int, from syscall.Sockaddr, err error)
 }
 
 type pinger struct {
@@ -66,44 +65,68 @@ func (p *pinger) pong(gpid int, fd int, out chan<- goping.RawResponse, donein <-
 	//Buffer to receive the ping packet
 	buf := make([]byte, 1024)
 
-	//Variables used in the for loop
-	var pid, seq int
+	//Buffer to receive the control message
+	oob := make([]byte, 64)
 
 	for {
+
 		//Receives a message from the socket sent by the kernel
+		if _, oobn, _, from, err := p.conn.Recvmsg(fd, buf, oob, 0); err != nil {
+			//Error on receiving packet
+		} else {
+			//Finds the pid and seq value.
+			var pid, seq int
+			switch buf[20] {
+			case 0:
+				//Received an Echo Reply
+				pid = int(uint16(buf[24])<<8 | uint16(buf[25]))
+				seq = int(uint16(buf[26])<<8 | uint16(buf[27]))
+			default:
+				//Received an error message
+				pid = int(uint16(buf[52])<<8 | uint16(buf[53]))
+				seq = int(uint16(buf[54])<<8 | uint16(buf[55]))
+				//emsg = msg[28:] //20+28+4
+			}
+			//Continue processing if pid == gpid and packet is not an EchoRequest.
+			if pid == gpid && buf[20] != 8 {
+				//Parses the Control Message to find the SO_TIMESTAMP value
+				if cmsgs, err := syscall.ParseSocketControlMessage(oob[:oobn]); err != nil {
+					//Error parsing socket control message
+				} else {
 
-		peer, when, err := p.conn.RecvMsg(fd, buf)
-		if err != nil {
-			log.Printf("Error reading icmp packet from the socket: %v\n", err)
-			continue //Continue on error
+					var bbuf bytes.Buffer
+					var tv syscall.Timeval
+					//Iterate over the control messages
+					for _, m := range cmsgs {
+						//Continue if control message is not syscall.SOL_SOCKET
+						if m.Header.Level != syscall.SOL_SOCKET {
+							continue
+						}
+						//Control Message is SOL_SOCKET, Verifyng if syscall is SO_TIMESTAMP
+						switch m.Header.Type {
+						case syscall.SO_TIMESTAMP:
+							bbuf.Write(m.Data)
+							binary.Read(&bbuf, binary.LittleEndian, &tv)
+							bbuf.Reset()
+							when := time.Unix(tv.Unix())
+							_ = when
+						}
+					}
+				}
+
+				//Get peer address
+				peer := net.IPv4(
+					from.(*syscall.SockaddrInet4).Addr[0],
+					from.(*syscall.SockaddrInet4).Addr[1],
+					from.(*syscall.SockaddrInet4).Addr[2],
+					from.(*syscall.SockaddrInet4).Addr[3],
+				)
+
+				//Send the raw response back to channel
+				out <- goping.RawResponse{Seq: seq, ICMPMessage: buf[:40], Peer: peer, RTT: 0}
+			}
 		}
-
-		//Finds the pid and seq value.
-		switch buf[20] {
-		case 8:
-			//Received an echo request. Probably from localhost. ignoring
-			continue
-		case 0:
-			//Received an Echo Reply
-			pid = int(uint16(buf[24])<<8 | uint16(buf[25]))
-			seq = int(uint16(buf[26])<<8 | uint16(buf[27]))
-		default:
-			//Received an error message
-			pid = int(uint16(buf[52])<<8 | uint16(buf[53]))
-			seq = int(uint16(buf[54])<<8 | uint16(buf[55]))
-			//emsg = msg[28:] //20+28+4
-		}
-
-		//Ignores processing if id is different from mypid
-		if pid != gpid {
-			continue
-		}
-
-		//Send the raw response back to channel
-		out <- goping.RawResponse{Seq: seq, ICMPMessage: buf[:40], Peer: peer, RTT: 0}
-
 	}
-
 }
 
 /***Conn Manager Implementation ***/
@@ -144,51 +167,7 @@ func (c *connmanager) Close(fd int) error {
 func (c *connmanager) SendTo(fd int, p []byte, to syscall.Sockaddr) error {
 	return syscall.Sendto(fd, p, 0, to)
 }
-func (c *connmanager) RecvFrom(fd int, buf []byte) (peer net.IP, when time.Time, rerr error) {
-
-	//Buffer to receive the control message
-	oob := make([]byte, 64)
-
-	//Receive Message from socket
-	n, oobn, recvflags, from, err := syscall.Recvmsg(fd, buf, oob, 0)
-	if err != nil {
-		rerr = err
-		return
-	}
-
-	//Parses the Control Message to find the SO_TIMESTAMP value
-	if cmsgs, err := syscall.ParseSocketControlMessage(oob[:oobn]); err != nil {
-		rerr = err
-		return
-	} else {
-
-		var bbuf bytes.Buffer
-		var tv syscall.Timeval
-		//Iterate over the control messages
-		for _, m := range cmsgs {
-			//Continue if control message is not syscall.SOL_SOCKET
-			if m.Header.Level != syscall.SOL_SOCKET {
-				continue
-			}
-			//Control Message is SOL_SOCKET, Verifyng if syscall is SO_TIMESTAMP
-			switch m.Header.Type {
-			case syscall.SO_TIMESTAMP:
-				bbuf.Write(m.Data)
-				binary.Read(&bbuf, binary.LittleEndian, &tv)
-				bbuf.Reset()
-				when = time.Unix(tv.Unix())
-			}
-		}
-	}
-
-	//Get peer address
-	peer = net.IPv4(
-		from.(*syscall.SockaddrInet4).Addr[0],
-		from.(*syscall.SockaddrInet4).Addr[1],
-		from.(*syscall.SockaddrInet4).Addr[2],
-		from.(*syscall.SockaddrInet4).Addr[3],
-	)
-
+func (c *connmanager) Recvmsg(fd int, p, oob []byte, flags int) (n, oobn int, recvflags int, from syscall.Sockaddr, err error) {
+	n, oobn, recvflags, from, err = syscall.Recvmsg(fd, p, oob, flags)
 	return
-
 }
